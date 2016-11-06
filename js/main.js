307a9b157f6ffaab4f4de91cc7cc7340977658a5
Benjamin Boruff <ben@boruff.me>
2016-11-06 01:53:06 +0000

    fixed async issue for ships elements

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var fwTest = {
     swFilms: [],
     removeCharList: [],
+    ships: [],
 
     /***
         fill selection dropdown with sw episodes ordered by release date
@@ -8,47 +9,50 @@ var fwTest = {
     ***/
     fillSelect: function() {
         // make ajax call to SWAPI to get all films
-        $.getJSON("https://swapi.co/api/films/", function(data) {
-            // push all film objects onto swFilms array
-            $.each(data.results, function(index, value) {
-                    fwTest.swFilms.push(value);
-                    //console.log(value.title);
-                })
-                // sort swFilms array based on release date
-            fwTest.swFilms.sort(function(a, b) {
-                if (a.release_date < b.release_date) {
-                    return -1;
-                }
-                if (a.release_date > b.release_date) {
-                    return 1;
-                }
-                // names must be equal
-                return 0;
-            });
-            // add each film title to drop down selector
-            $.each(fwTest.swFilms, function(index, film) {
-                $("#film-titles").append('<li><a href="#"><data value="' +
-                    film.url + '">' + film.title + '</data></a></li>');
-            });
-
-            // when a film is selected from the drop down, query SWAPI for title
-            // to get characters
-            $("#film-titles li").click(function(e) {
-                // clear old results
-                $("#sw-table").empty();
-                $("#ep-title").html($(this).html());
-                var titleUrl = $(this).find('data').attr('value');
-                titleUrl = fwTest.secureUrl(titleUrl);
-                var characters = [];
-                //console.log(titleUrl);
-                $.getJSON(titleUrl, function(data) {
-                    characters = data.characters;
-                    //console.log(characters);
-                    fwTest.createCharacterTable(characters);
+        $.when($.getJSON("https://swapi.co/api/films/"))
+            .then(function(data, textStatus, jqXHR) {
+                // push all film objects onto swFilms array
+                $.each(data.results, function(index, value) {
+                        fwTest.swFilms.push(value);
+                        //console.log(value.title);
+                    })
+                    // sort swFilms array based on release date
+                fwTest.swFilms.sort(function(a, b) {
+                    if (a.release_date < b.release_date) {
+                        return -1;
+                    }
+                    if (a.release_date > b.release_date) {
+                        return 1;
+                    }
+                    // names must be equal
+                    return 0;
                 });
-            });
+                // add each film title to drop down selector
+                $.each(fwTest.swFilms, function(index, film) {
+                    $("#film-titles").append('<li><a href="#"><data value="' +
+                        film.url + '">' + film.title + '</data></a></li>');
+                });
 
-        });
+                // when a film is selected from the drop down, query SWAPI for title
+                // to get characters
+                $("#film-titles li").click(function(e) {
+                    // clear old results
+                    $("#sw-table").empty();
+                    $("#ep-title").html($(this).html());
+                    var titleUrl = $(this).find('data').attr('value');
+                    titleUrl = fwTest.secureUrl(titleUrl);
+                    var characters = [];
+                    //console.log(titleUrl);
+                    $.getJSON(titleUrl)
+                        .done(function(data) {
+                            characters = data.characters;
+                            //console.log(characters);
+                            fwTest.createCharacterTable(characters);
+                        });
+                });
+
+            });
+        //*** end async
     },
 
     //*** I develope from https, so I have to change the url's http to https.
@@ -61,10 +65,11 @@ var fwTest = {
         // make axaj call for each character
         $.each(charArr, function(index, charUrl) {
             charUrl = fwTest.secureUrl(charUrl);
-            $.getJSON(charUrl, function(char) {
-                // add a row with character name and starships if not removed
-                $("#sw-table").append(fwTest.makeRow(char.name, char.starships));
-            });
+            $.when($.getJSON(charUrl))
+                .then(function(char) {
+                    // add a row with character name and starships if not removed
+                    $("#sw-table").append(fwTest.makeRow(char.name, char.starships));
+                });
         });
     },
 
@@ -96,7 +101,7 @@ var fwTest = {
         // add list of starships to row if they have any
         if (shipArr.length !== 0) {
             //console.log(charName + " has ships!");
-            $("#" + shipDataId).html(fwTest.getShips(shipArr).join(", "));
+            fwTest.getShips(shipDataId, shipArr);
         }
     },
 
@@ -106,22 +111,28 @@ var fwTest = {
      of time. This needs to be refactored into an async solution
      to improve perfomance; the UI suffers for it, otherwise.
     ***/
-    getShips: function(shipArr) {
-        var ships = [];
-        $.ajaxSetup({
-            async: false
-        });
+    getShips: function(shipDataId, shipArr) {
+        var promises = [];
+
+        function fetchShips(url) {
+            return $.getJSON(url).then(function(data) {
+                return data;
+            });
+        }
         for (var i = 0; i < shipArr.length; i++) {
             var starshipUrl = fwTest.secureUrl(shipArr[i]);
             //console.log(starshipUrl);
-            $.getJSON(starshipUrl, function(ship) {
-                ships.push(ship.name);
-            });
+            promises.push(fetchShips(starshipUrl));
         }
-        $.ajaxSetup({
-            async: true
-        });
 
-        return ships;
+        $.when.apply(this, promises).then(function() {
+            var shipArray = Array.from(arguments);
+            shipArray.forEach(function(ship) {
+                //console.log(ship.name);
+                fwTest.ships.push(ship.name);
+            });
+            $("#" + shipDataId).html(fwTest.ships.join(", "));
+            fwTest.ships = [];
+        });
     }
 };
